96691016a3a8a4b235efc3ed11ca8ba99b0d5126
code2tan <180329416@qq.com>
2023-10-13 01:14:38 +0800

    LC 416

---
+++ src/org/algorithm/graph/BagProblem.java
@@ -0,0 +1,43 @@
+package org.algorithm.graph;
+
+import java.util.Arrays;
+
+public class BagProblem {
+    /**
+     * 动态规划获得结果
+     *
+     * @param weight  物品的重量
+     * @param value   物品的价值
+     * @param bagSize 背包的容量
+     */
+    public static void testWeightBagProblem(int[] weight, int[] value, int bagSize) {
+        // 创建dp数组
+        int goods = weight.length;
+        int[][] dp = new int[goods][bagSize + 1];
+        // 初始化dp数组
+        for (int j = weight[0]; j <= bagSize; j++) {
+            dp[0][j] = value[0];
+        }
+        // 填充dp数组
+        for (int i = 1; i < weight.length; i++) {
+            for (int j = 1; j <= bagSize; j++) {
+                if (j < weight[i]) {
+                    dp[i][j] = dp[i - 1][j];
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - weight[i]] + value[i]);
+                }
+            }
+        }
+        // 打印dp数组
+        for (int[] arr : dp) {
+            System.out.println(Arrays.toString(arr));
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] weight = {1, 3, 4};
+        int[] value = {15, 20, 30};
+        int bagSize = 4;
+        testWeightBagProblem(weight, value, bagSize);
+    }
+}
---
+++ src/org/algorithm/graph/Solution416.java
@@ -0,0 +1,24 @@
+package org.algorithm.graph;
+
+class Solution416 {
+    public boolean canPartition(int[] nums) {
+        int sum = 0;
+        int index = 0;
+        while (index < nums.length) {
+            sum += nums[index++];
+        }
+        if (sum % 2 != 0) return false;
+        int target = sum / 2;
+        int[] dp = new int[target + 1];
+        for (int num : nums) {
+            for (int j = target; j >= num; j--) {
+                dp[j] = Math.max(dp[j], dp[j - num] + num);
+            }
+        }
+        return dp[target] == target;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
