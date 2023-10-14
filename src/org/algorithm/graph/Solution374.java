91310c192ac560cf8da4c0cc33c96c1e35d675b9
code2tan <180329416@qq.com>
2023-10-15 00:45:33 +0800

    LC374

---
+++ src/org/algorithm/graph/Solution374.java
@@ -0,0 +1,27 @@
+package org.algorithm.graph;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.PriorityQueue;
+
+class Solution374 {
+    public int[] topKFrequent(int[] nums, int k) {
+        // 大顶堆
+        PriorityQueue<int[]> priorityQueue = new PriorityQueue<>(k, (a, b) -> b[1] - a[1]);
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int num : nums) {
+            map.putIfAbsent(num, 0);
+            map.computeIfPresent(num, (key, oldValue) -> ++oldValue);
+        }
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            priorityQueue.add(new int[]{entry.getKey(), entry.getValue()});
+        }
+        int[] res = new int[k];
+        for (int i = 0; i < k; i++) {
+            res[i] = Objects.requireNonNull(priorityQueue.poll())[0];
+        }
+        return res;
+    }
+}
