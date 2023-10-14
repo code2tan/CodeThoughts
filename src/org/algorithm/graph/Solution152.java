50b2fd339b907dfa48911449bbe3d210fdda8a16
code2tan <180329416@qq.com>
2023-10-14 09:13:01 +0800

    LC152

---
+++ src/org/algorithm/graph/Solution152.java
@@ -0,0 +1,21 @@
+package org.algorithm.graph;
+
+class Solution152 {
+
+    public int maxProduct(int[] nums) {
+        int result = Integer.MIN_VALUE;
+        int max = nums[0], min = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            int tempMax = max, tempMin = min;
+            max = Math.max(Math.max(nums[i] * tempMax, nums[i] * tempMin), nums[i]);
+            min = Math.min(Math.min(nums[i] * tempMax, nums[i] * tempMin), nums[i]);
+            result = Math.max(max, result);
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = new int[]{2, 3, -2, 4};
+        System.out.println(new Solution152().maxProduct(arr));
+    }
+}
