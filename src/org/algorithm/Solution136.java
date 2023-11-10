d3f7d3dc21c5a9e879d826ccdba57275ad0a5647
code2tan <180329416@qq.com>
2023-11-10 14:51:35 +0800

    LC 136

---
+++ src/org/algorithm/Solution136.java
@@ -0,0 +1,11 @@
+package org.algorithm;
+
+class Solution136 {
+    public int singleNumber(int[] nums) {
+        int result = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            result ^= nums[i];
+        }
+        return result;
+    }
+}
